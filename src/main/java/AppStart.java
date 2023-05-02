5ed5baf111dd5ffdf75e7544b597c5e97f291efa
jiho <sulfur0525@naver.com>
2023-05-02 14:47:01 +0900

    Initial commit

---
+++ src/main/java/AppStart.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication // 하위폴더내 컴포넌트스캔 빈 등록
+@EnableJpaAuditing
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+    }
+}
