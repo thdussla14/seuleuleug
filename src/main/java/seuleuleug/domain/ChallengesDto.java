d6b1cdcf2299ebe5f2307b1238b4af4e56e00c14
jiho <sulfur0525@naver.com>
2023-05-02 17:42:21 +0900

    2023.05.02

---
+++ src/main/java/seuleuleug/domain/ChallengesDto.java
@@ -0,0 +1,27 @@
+package seuleuleug.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChallengesDto {
+    private int chno;
+    private String chname;
+    private String chcontent;
+    private String chimg;
+
+    // toEntity
+    public ChallengesEntity toEntity() {
+        return ChallengesEntity.builder()
+            .chno(this.chno)
+            .chname(this.chname)
+            .chcontent(this.chcontent)
+            .chimg(this.chimg)
+            .build();
+    }
+
+}
---
+++ src/main/java/seuleuleug/domain/ChallengesEntity.java
@@ -0,0 +1,36 @@
+package seuleuleug.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "challenges")
+public class ChallengesEntity {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private int chno;
+    @Column
+    private String chname;
+    @Column
+    private String chcontent;
+    @Column
+    private String chimg;
+
+    // toDto 출력용
+    public ChallengesDto todto(){
+        return ChallengesDto.builder()
+            .chno(this.chno)
+            .chname(this.chname)
+            .chcontent(this.chcontent)
+            .chimg(this.chimg)
+            .build();
+    }
+
+}
