95638b7bb6006dba4cd69716a06290c4aab2f044
thdussla14 <judy1777@naver.com>
2023-05-03 15:48:06 +0900

    2023-05-03 라우터 설정

--- src/main/frontend/src/component/Index.js
+++ src/main/frontend/src/component/Index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter , Routes , Route } from 'react-router-dom';
 import Header from './Header';
-import Footer from './Footer';
 import Main from './Main';
 
 
@@ -12,7 +11,6 @@ export default function Index(props) {
             <Routes >
                 <Route path="/" element={<Main />} />
             </Routes>
-            <Footer />
         </BrowserRouter>
     </>)
 }
