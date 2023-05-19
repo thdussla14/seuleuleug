0ef1c8954d460a46878cefb4ac76202a111b56ce
권태형 <katonew98@gmail.com>
2023-05-19 12:20:35 +0900

    통합

--- src/main/frontend/src/component/chatting/ChatRoom.js
+++ src/main/frontend/src/component/chatting/ChatRoom.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import doctor from '../../doctor.PNG';
+import doctor from '../../doctor.png';
 import Avatar from "@mui/material/Avatar";
 
 
